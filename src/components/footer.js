3f3b205526dda76f89c92f7c0f2684491828c2ba
Tyler Sackel <78436840+BiziLife@users.noreply.github.com>
2024-03-19 21:21:41 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,16 +3,10 @@ export default function Navbar() {
   return (
       <footer className="footer p-10 bg-base-200 text-base-content">
       <aside>
-        <img className="h-12 w-12" src="../favicon.ico"></img>
-        <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+        {/*I want image here*/}
+        <img className="h-20 w-auto" src="/SER_Logo.png" alt="SER Logo" />
+        <p>Software Engineering Readiness<br/>Creating fantastic websites scence 2023!</p>
       </aside> 
-      <nav>
-        <a href="https://www.liberty.edu/"><h6 className="footer-title link link-hover">Liberty University </h6></a>
-        <a href="https://www.liberty.edu/business/bachelors/computer-science/" className="link link-hover">B.S. in Computer Science (Residential)</a>
-        <a href="https://www.liberty.edu/online/business/bachelors/computer-science-cybersecurity/" className="link link-hover">B.S. in Computer Science-Cybersecurity(Online)</a>
-        <a href="https://www.liberty.edu/residential/business/bachelors/computer-science/software-engineering/" className="link link-hover">Software Engineering</a>
-        <a href="https://www.liberty.edu/business/bachelors/computer-science/web-mobile-programming/" className="link link-hover">Web & Mobile Programming</a>
-      </nav> 
       <nav>
         <h6 className="footer-title">SERC</h6> 
         <a className="link link-hover">About us</a>
@@ -20,6 +14,15 @@ export default function Navbar() {
         <a className="link link-hover">Jobs</a>
         <a className="link link-hover">Press kit</a>
       </nav> 
+
+      <nav>
+        <a href="https://www.liberty.edu/"><h6 className="footer-title link link-hover">Liberty University </h6></a>
+        <a href="https://www.liberty.edu/business/bachelors/computer-science/" className="link link-hover">B.S. in Computer Science (Residential)</a>
+        <a href="https://www.liberty.edu/online/business/bachelors/computer-science-cybersecurity/" className="link link-hover">B.S. in Computer Science-Cybersecurity(Online)</a>
+        <a href="https://www.liberty.edu/residential/business/bachelors/computer-science/software-engineering/" className="link link-hover">Software Engineering</a>
+        <a href="https://www.liberty.edu/business/bachelors/computer-science/web-mobile-programming/" className="link link-hover">Web & Mobile Programming</a>
+      </nav> 
+
       <nav>
         <h6 className="footer-title">Legal</h6> 
         <a className="link link-hover">Terms of use</a>
