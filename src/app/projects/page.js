20a83e4107f7df9794efb2e9f2d606b9e155877e
4ubiks <jaharris20@liberty.edu>
2024-03-26 17:46:31 -0400

    making page cohesive to web browser size

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -9,40 +9,37 @@ export default function Projects(){
 
           <Navbar></Navbar>
 
-          <div className="w-full">                      
-              <div class="flex items-center justify-center bg-gradient-to-r from-indigo-950 py-16">
-                <div class="w-max">
+          <div className="w-auto">                      
+              <div class="flex items-center justify-center bg-gradient-to-r from-indigo-950 lg:py-16 md:py-8 sm:py-4">
+                <div class="w-auto">
                     <h1
-                    class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-7 text-9xl text-white">
+                    class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-7 pt-4 lg:text-9xl md:text-5xl sm:text-3xl text-white">
                     current projects
                     </h1>
                 </div>
                </div>
 
-              <div className={"bg-gradient-to-r from-indigo-950 w-full flex justify-center py-28"}>
+              <div className={"bg-gradient-to-r from-indigo-950 w-full flex justify-center py-8"}>
                   <div className={"w-11/12 max-w-[2500px] grid grid-rows-3 gap-3.5"}>
 
                     {/* First box */}
                       <div className={"row-start-1"}>
                           <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center transition-all duration-300 filter grayscale hover:grayscale-0">
                               <div className="avatar">
-                                  <div className="w-72 rounded">
-                                    <Images className="5x1 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer ease-in-out hover:scale-110" src="/programming-skills.png" alt="website's under construction!" height={500} width={500} />
-
-                                    {/*  <img className="rounded 5x1 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer ease-in-out hover:scale-110" src="https://www.springboard.com/blog/wp-content/uploads/2022/08/programming-skills.png" alt="website's under construction!"/> */}
-                                     
+                                  <div className="lg:w-72 rounded">
+                                  <Images className="5x1 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer ease-in-out hover:scale-110" src="/programming-skills.png" alt="brain" height={500} width={500} />
                                   </div>
                               </div>
 
                               <div className={"ml-8 h-full py-12 prose lg:prose-md"}>
-                                  <h1 class="text-5xl">
+                                  <h1 class="lg:text-5xl md:text-3xl sm:text-xl">
                                     Club Website                          
                                   </h1>
 
-                                  <p class="text-2xl text-gray-200 py-8">The most vital element of an organization is recognizability. We are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do.</p>
+                                  <p class="lg:text-2xl md:text-xl sm:text-m text-gray-200 py-8">The most vital element of an organization is recognizability. We are currently in development of a visually pleasing, informative website that caters to anyone who may want to know more about what we do.</p>
                                  
                                   <div p-10>
-                                    <button href="https://www.google.com" target="_blank" class="btn btn-active text-sky-100 bg-red-500 hover:bg-red-700">useless button</button>
+                                    <button class="btn btn-active text-sky-100 bg-red-500 hover:bg-red-700"><a href="https://ser-website.vercel.app" target="_blank" >Useless Button</a></button>
                                 </div>
                               </div>
 
@@ -53,17 +50,17 @@ export default function Projects(){
                       <div className={"row-start-2 py-4"}>
                           <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center flex-row-reverse justify-between transition-all duration-300 filter grayscale hover:grayscale-0">
                               <div className="avatar">
-                                  <div className="w-72 rounded">
+                                  <div className="lg:w-72 rounded">
                                   <Images className="5x1 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer ease-in-out hover:scale-110" src="/rust.png" alt="website's under construction!" height={500} width={500} />
                                   </div>
                               </div>
 
                               <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
-                                  <h1 class="text-5xl">
+                                  <h1 class="lg:text-5xl md:text-3xl sm:text-xl">
                                     Rust Proof-of-Concept
                                   </h1>
 
-                                  <p class="text-2xl py-8">
+                                  <p class="lg:text-2xl md:text-xl sm:text-m py-8">
                                   Utilizing an impressive array of miscellaneous utility and recreational Rust scripts, perfect for mastering the practical applications of the language. 
                                   </p>
                                         
@@ -80,17 +77,17 @@ export default function Projects(){
                       <div className={"row-start-3 py-4"}>
                           <div className="bg-base-100 w-full h-[27rem] rounded-3xl p-16 flex items-center transition-all duration-300 filter grayscale hover:grayscale-0">
                               <div className="avatar">
-                                  <div className="w-72 rounded">
+                                  <div className="lg:w-72 rounded">
                                   <Images className="5x1 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer ease-in-out hover:scale-110" src="/network1.jpg" alt="brain" height={500} width={500} />
                                   </div>
                               </div>
 
                               <div className={"ml-8 h-full py-8 prose lg:prose-md"}>
-                                  <h1 class="text-5xl">
+                                  <h1 class="lg:text-5xl md:text-3xl sm:text-xl">
                                     Next Projects
                                   </h1>
 
-                                  <p class="text-2xl py-8">
+                                  <p class="lg:text-2xl md:text-xl sm:text-m py-8">
                                     What kind of projects would you like to see? Leave an idea below!
                                   </p>
                                   
